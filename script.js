5bceaff81234f4709ad079f9eea729137b32b835
isabellysantanamelo-ux <isabelly.santana.melo@escola.pr.gov.br>
2026-08-28 10:48:18 -0300

    Update script.js

--- script.js
+++ script.js
@@ -257,13 +257,13 @@ function trocarImagem(escolha) {
         case "1":
           urlNova = 'café.png';
           textoprincipal.innerText="Como explicar o Sol?";
-          texto.innerText="O Sol é uma estrela gigante feita de gás quente que fica no centro do nosso Sistema Solar, a 150 milhões de quilômetros da Terra. Ele dá luz e calor ao nosso planeta, controla a gravidade que segura os planetas ao seu redor e é essencial para a existência de toda a vida.";
+          texto.innerText="paragrafo";
           descrevefigura.innerText="Imagem do Sol na cor amarela ";
           break;
         case "2":
           urlNova = 'cordosol.jfif';
           textoprincipal.innerText="A verdadeira cor do SOL ";
-          texto.innerText="O Sol visto a olho nu no espaço é na verdade branco. A coloração amarela que vemos aqui na superfície da Terra é por causa da atmosfera da Terra.";
+          texto.innerText="paragrafo";
           descrevefigura.innerText="Imagem do Sol na cor branca ";
           break;
         default:
